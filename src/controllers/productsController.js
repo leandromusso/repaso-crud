e000bbf315d740c2abec6f41dd1e42e8f967dc88
Leandro Musso <leandro@trama.solutions>
2022-10-26 21:05:52 -0300

    ❤️

---
+++ src/controllers/productsController.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	// Root - Show all products
+	index: (req, res) => {
+		res.render('products', {products : products});
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		// Do the magic
+	},
+
+	// Create - Form to create
+	create: (req, res) => {
+		res.render('product-create-form');
+	},
+	
+	// Create -  Method to store
+	store: (req, res) => {
+		
+		let newProduct = {
+			id: Date.now(),
+			name : req.body.name,
+			price : Number(req.body.price),
+			discount : Number(req.body.discount),
+			category : req.body.category,
+			description : req.body.description,
+			image : req.body.image
+		}
+
+		products.push(newProduct);
+
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '));
+
+		res.redirect('/products');
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+		// Do the magic
+	},
+	// Update - Method to update
+	update: (req, res) => {
+		// Do the magic
+	},
+
+	// Delete - Delete one product from DB
+	destroy : (req, res) => {
+		// Do the magic
+	}
+};
+
+module.exports = controller;
